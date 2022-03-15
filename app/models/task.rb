e0c1d652413d140620beb9f21ec8c1952f6a0155
Kashaf <kashaflareb@gmail.com>
2022-03-15 10:48:56 +0500

    Add aasm_state to Tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,16 @@
 class Task < ApplicationRecord
+  # include AASM
+
+  # aasm do # default column: aasm_state
+  #   state :sleeping, initial: true
+  #   state :running
+
+  #   event :run do
+  #     transitions from: :sleeping, to: :running
+  #   end
+
+  #   event :sleep do
+  #     transitions from: :running, to: :sleeping
+  #   end
+  # end
 end
